6a11bdb2af4175b932eb619e850cb9a3b59936b5
jiinjung <jiinrosejung@gmail.com>
2023-06-20 02:51:01 -0500

    changed config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: './playground',
     server:
     {
         host: true,
